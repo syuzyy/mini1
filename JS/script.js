08f3d3d18892b644fcde5312fd9cd8405011b2d4
syuzyy <140905572+syuzyy@users.noreply.github.com>
2023-08-13 16:25:21 +0400

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+//alert('Hello World');
+//document.write('Hello World');
+let x  = 2.1;
+let y = 1.6;
+console.log(x)
+console.log(Math.ceil(x));
+console.log(Math.floor(x));
+console.log(Math.round(x));
+console.log(Math.round(Math.random()*5))
